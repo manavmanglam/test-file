a69977188422488cbd4ed6e789c4e9ef5f8a5f1a
MANAV MANGLAM <145098878+manavmanglam@users.noreply.github.com>
2024-12-14 15:22:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+// "static void main" must be defined in a public class.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("MANAV MANGLAM");
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+        int d=10;
+        int e=20;
+        int f=e-d;
+        System.out.println(f);
+        int g=30;
+        int h=20;
+        int j=g/h;
+        System.out.println(j);
+        int o=30;
+        int i=40;
+        int t=o*i;
+        System.out.println(t);
+        
+    }
+}
